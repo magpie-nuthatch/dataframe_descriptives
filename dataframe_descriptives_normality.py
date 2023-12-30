0e5c3414f6efb055bf2f46cdca1b36337331ed61
t99forever <ybezema@gmail.com>
2023-12-30 14:30:17 +0100

    Made unique values descriptions and normality tests optional

--- dataframe_descriptives_normality.py
+++ dataframe_descriptives_normality.py
@@ -2,26 +2,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as stats
 
-def descriptives(df, unique_values_max_count, plot_bin_amount):
+def descriptives(df, unique_values_bool, unique_values_max_count, normality_tests_bool, normality_tests_bins):
     np.set_printoptions(suppress = True,
                     formatter = {'float_kind':'{:0.2f}'.format})
     try:
         print()
-        print(f"Information for columns in current dataframe:")
+        print(f"Information on columns in current dataframe:")
         print(df.info())
         for column in df:
             df_loop = df.dropna(subset = column)
             print()
             print(f"Descriptives for column {column}:")
             print(df_loop[column].describe())
-            if df_loop[column].dtype == "object" and len(df_loop[column].unique()) < unique_values_max_count:
+            if df_loop[column].dtype == "object" and unique_values_bool == True and len(df_loop[column].unique()) < unique_values_max_count:
                 unique_values = sorted(df_loop[column].unique())
                 print()
                 print(f"Unique values in column {column}:")
                 for value in unique_values:
                     print(value)
+            else:
+                pass
             print()
-            if df_loop[column].dtype != "object":
+            if df_loop[column].dtype != "object" and normality_tests_bool == True:
                 try:
                     fig = plt.figure(figsize = (10, 10))
                     ax = fig.add_subplot(111)
@@ -33,27 +35,22 @@ def descriptives(df, unique_values_max_count, plot_bin_amount):
 
                     df_loop.plot(kind = "hist", 
                                 y = column, 
-                                bins = plot_bin_amount, 
+                                bins = normality_tests_bins, 
                                 title = f"Histogram for {column}")
                     plt.show()
 
-                    print()
                     print(f"Statistical normality test for {column}")
                     print(stats.normaltest(df_loop[column]))
-                    print()
-                    print(f"Descriptives for column {column}:")
-                    print(df_loop[column].describe())
                 except:
                     try:
-                        print()
                         print(f"Statistical normality test for {column}")
                         print(stats.normaltest(df_loop[column]))
-                        print()
-                        print(f"Descriptives for column {column}:")
-                        print(df_loop[column].describe())
                     except AttributeError:
                         print("Normality tests unavailable for current column.")
                     except:
                         pass
+                print()
+            else:
+                pass
     except AttributeError:
         print("Descriptives unavailable for current dataframe.")
